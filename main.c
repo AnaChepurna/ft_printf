3946b43d7043619721931a044c4c9bca4ca21945
Anastasia CHEPURNA <achepurn@e2r12p7.unit.ua>
2018-05-09 17:14:58 +0300

    g

--- main.c
+++ main.c
@@ -6,6 +6,7 @@ int main(int argc, char const *argv[])
 	//ft_printf("%10.7s\n", "love love love");
 		int x1 = ft_printf("{%g}", 2147480000.0);
 	int x2 = printf("{%g}", 2147480000.0);
+	ft_printf("{%g}", 0.0);
 	// int x1 = ft_printf("{%S}", L"привет, Дiiiiiима");
 	// int x2 = printf("{%S}", L"привет, Дiiiiiима");
 	printf("1:%i, 2:%i\n", x1, x2);
